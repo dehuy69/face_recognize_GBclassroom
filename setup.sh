9fa70d0638ff8140c980df4cd34c2d5de07eec7b
dehuy69 <dr.huy69@gmail.com>
2018-10-08 21:23:52 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo apt-get install picamera python-numpy python-scipy
-pip install face_recognition
+sudo apt-get install python-picamera python3-picamera python-numpy python3-numpy python-scipy python3-scipy python-opencv python3-opencv cmake python-dev python3-dev
+sudo pip install face_recognition
